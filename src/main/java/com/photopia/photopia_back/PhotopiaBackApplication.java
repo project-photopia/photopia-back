25d360ab7a8395988b5afbc9b125d1a5e2e107e7
Bulbizar <adrienesteves@icloud.com>
2025-10-13 11:26:14 +0200

    spring boot init

---
+++ src/main/java/com/photopia/photopia_back/PhotopiaBackApplication.java
@@ -0,0 +1,13 @@
+package com.photopia.photopia_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotopiaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotopiaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/photopia/photopia_back/PhotopiaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photopia.photopia_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotopiaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
