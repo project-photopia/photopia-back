ca3e460d95e57fa44193336b2aaa00bd609f3e12
Bulbizar <adrienesteves@icloud.com>
2026-02-11 12:11:03 +0100

    add deep-link

--- src/main/java/com/photopia/photopia_back/repository/CapsuleRepository.java
+++ src/main/java/com/photopia/photopia_back/repository/CapsuleRepository.java
@@ -14,4 +14,6 @@ public interface CapsuleRepository extends JpaRepository<Capsule, UUID> {
     Optional<Capsule> findByJoinToken(String joinToken);
 
     List<Capsule> findByUser(User user);
+
+    Optional<Capsule> findByIdAndUser_Id(UUID id, UUID userId);
 }
