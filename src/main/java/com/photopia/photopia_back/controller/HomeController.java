e9007f7c32f3e678b0405c5fc72aedb5b0dbc5c7
Louis YANG <louis.yang@animedigitalnetwork.fr>
2026-02-11 14:55:41 +0100

    [fix] fixing copilot review commentary

--- src/main/java/com/photopia/photopia_back/controller/HomeController.java
+++ src/main/java/com/photopia/photopia_back/controller/HomeController.java
@@ -1,14 +1,18 @@
 package com.photopia.photopia_back.controller;
 
+import java.net.URI;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class HomeController {
 
     @GetMapping("/")
-    public RedirectView home() {
-        return new RedirectView("/swagger-ui/index.html");
+    public ResponseEntity<Void> home() {
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create("/swagger-ui/index.html"))
+                .build();
     }
 }
