a7b48a04d7b1f9ccbb64141f90e27edbe5f10c18
Louis YANG <louis.yang@animedigitalnetwork.fr>
2026-02-17 21:28:31 +0100

    [FIX] Fixing redis error

--- src/main/java/com/photopia/photopia_back/service/UserService.java
+++ src/main/java/com/photopia/photopia_back/service/UserService.java
@@ -33,7 +33,7 @@ public class UserService {
         return repo.findAll();
     }
 
-    @Cacheable(value = "users", key = "#id")
+    @Cacheable(value = "users_v1", key = "#id")
     public User getUserById(UUID id) {
         return repo.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
     }
@@ -81,7 +81,7 @@ public class UserService {
         return data;
     }
 
-    @CacheEvict(value = "users", key = "#user.id")
+    @CacheEvict(value = "users_v1", key = "#user.id")
     public User updateUser(User user) {
         return repo.save(user);
     }
